4aa4de8f54f3b3178d5c631f9fc5371e057bc169
osilverstein <silversteinoliver@gmail.com>
2021-11-02 19:32:21 -0400

    finish mock

--- test/ControllerTest.java
+++ test/ControllerTest.java
@@ -1,5 +1,5 @@
 public class ControllerTest {
-  //jsut make sure that the inputs are getting pass to the model correctly
+  //just make sure that the inputs are getting pass to the model correctly
   //within our new controller mock, have field that is an appendable log
   //all of our methods are going to log the appendable and compare them to make sure
   //that the correct methods are being passed
